77e928080e72fb6d861f8ba96af945c11b8ec283
Aiden McDougal <aidenmcdougal@icloud.com>
2023-09-19 20:21:30 -0400

    Initial Commit

---
+++ Vulcan/VulcanApp.swift
@@ -0,0 +1,17 @@
+//
+//  VulcanApp.swift
+//  Vulcan
+//
+//  Created by Aiden McDougal on 9/19/23.
+//
+
+import SwiftUI
+
+@main
+struct VulcanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
