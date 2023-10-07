d500f90225cb298f78cb1d058485904902442869
RaiJashan17 <125523346+RaiJashan17@users.noreply.github.com>
2023-10-07 11:10:47 -0400

    Update Firebase.py
    Firebase is now able to use the json file to update the database.

--- Firebase.py
+++ Firebase.py
@@ -3,20 +3,15 @@ from firebase_admin import credentials
 from firebase_admin import db
 import json
 
-# Initialize Firebase Admin SDK with your service account key
-cred = credentials.Certificate("/Users/jrai/Documents/GitHub/Vulcan/vulcan-203a4-firebase-adminsdk-j79bb-014b7ab941.json")  # Replace with the path to your service account key JSON file
+# Fetch the service account key JSON file contents
+cred = credentials.Certificate('/Users/jrai/Documents/GitHub/Vulcan/vulcan-203a4-firebase-adminsdk-j79bb-014b7ab941.json')
+# Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://vulcan-203a4.firebaseio.com'  # Replace with your Firebase Realtime Database URL
+    'databaseURL': "https://vulcan-203a4-default-rtdb.firebaseio.com/"
 })
 
-# Read the JSON data from the existing JSON file
-with open('/Users/jrai/Documents/GitHub/Vulcan/file_data.json', 'r') as json_file:
-    json_data = json.load(json_file)
-
-# Reference to the database location where you want to store the data
-ref = db.reference("/")  # Replace with the desired path in your database
-
-# Push the data to the specified location
-new_entry = ref.push(json_data)
-print(f"Data uploaded with key: {new_entry.key}")
+ref = db.reference('/')
+with open("/Users/jrai/Documents/GitHub/Vulcan/file_data.json", "r") as f:
+	file_contents = json.load(f)
+ref.set(file_contents)
 
