ae229cb9c8c234a9d86d79078f5de9390ef6c15d
RaiJashan17 <125523346+RaiJashan17@users.noreply.github.com>
2023-10-07 10:34:12 -0400

    Coverting CSV to JSON file

--- Data.py
+++ Data.py
@@ -1,30 +1,51 @@
 import requests
+import csv
 import json
+import os  # Import the 'os' module for working with file paths
 
 # Define the API URL
-url = "https://firms.modaps.eosdis.nasa.gov/api/country/json/39698316900e4e7fc292375d74aa88f1/LANDSAT_NRT/USA/1/2023-09-19"
+url = "https://firms.modaps.eosdis.nasa.gov/api/country/csv/39698316900e4e7fc292375d74aa88f1/LANDSAT_NRT/USA/1/2023-09-19"
+
+# Specify the directory where you want to save the file
+save_directory = "/Users/jrai/Documents/GitHub/Vulcan"  # Replace with the actual directory path
 
 # Make the API request
 response = requests.get(url)
 
 # Check if the request was successful (status code 200)
 if response.status_code == 200:
-    # Check if the response contains data (not empty)
-    if response.text.strip():
-        try:
-            # Attempt to parse the JSON response data
-            json_data = response.json()
+    # Parse the CSV response data
+    csv_data = response.text
 
-            # Save the data to a JSON file
-            with open('fire_data.json', 'w') as json_file:
-                json.dump(json_data, json_file, indent=4)
+    # Specify the full file path
+    file_path = os.path.join(save_directory, 'fire_data.csv')
 
-            print("Fire data saved to 'fire_data.json'")
-        except json.JSONDecodeError as e:
-            print(f"Error parsing JSON: {e}")
-    else:
-        print("The API response is empty.")
+    # Save the data to a CSV file in the specified directory
+    with open(file_path, 'w', newline='') as csv_file:
+        csv_file.write(csv_data)
+
+    print(f"Fire data saved to '{file_path}'")
 else:
     print(f"Error: {response.status_code} - {response.text}")
 
+
+# Read CSV data from a file
+csv_file_path = '/Users/jrai/Documents/GitHub/Vulcan/fire_data.csv'  # Replace with the path to your CSV file
+json_file_path = '/Users/jrai/Documents/GitHub/Vulcan/file_data.json'  # Output JSON file path
+
+csv_data = []
+
+with open(csv_file_path, 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        csv_data.append(row)
+
+# Convert CSV data to JSON
+json_data = json.dumps(csv_data, indent=4)
+
+# Save the JSON data to a file
+with open(json_file_path, 'w') as json_file:
+    json_file.write(json_data)
+
+print(f"CSV data converted to JSON and saved to '{json_file_path}'")
 
--- Firebase.py
+++ Firebase.py
@@ -4,7 +4,7 @@ from firebase_admin import db
 
 cred = credentials.Certificate("/Users/jrai/Downloads/vulcan-203a4-firebase-adminsdk-j79bb-014b7ab941.json")
 firebase_admin.initialize_app(cred)
-ref = db.reference('/path/to/your/data')
+ref = db.reference('/Users/jrai/Documents/GitHub/Vulcan/file_data.json')
 
 # Read data from the database
 data = ref.get()
